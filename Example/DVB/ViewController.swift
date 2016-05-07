320efa5046bef59bda38172f345c467b94d3c6c0
Kilian Koeltzsch <me@kilian.io>
2016-05-07 03:32:10 +0200

    Turn Example into something marginally useful

--- Example/DVB/ViewController.swift
+++ Example/DVB/ViewController.swift
@@ -9,14 +9,63 @@
 import UIKit
 import DVB
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+
+    var connections: [Connection]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        DVB.monitor("Pirnaischer Platz", limit: 5, line: "3") { (connections) in
-            for con in connections {
-                print(con)
-            }
+        refresh()
+    }
+
+    func refresh() {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+
+        DVB.monitor("Helmholtzstraße", limit: 10, line: "85") { (connections) in
+            self.connections = connections
+            dispatch_async(dispatch_get_main_queue(), {
+                [unowned self] in
+                UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+                self.tableView.reloadData()
+            })
         }
+    }
+
+    @IBAction func refreshButtonTapped(sender: UIBarButtonItem) {
+        refresh()
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let connections = connections else { return 0 }
+        return connections.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("connectionCell") ?? UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "connectionCell")
+
+        guard let connections = connections else { return cell }
+
+        let connection = connections[indexPath.row]
+
+        cell.textLabel?.text = "\(connection.line) \(connection.direction)"
+
+        var detail = ""
+        if connection.minutesUntil == 0 {
+            detail = "now"
+        } else {
+            detail = "\(connection.minutesUntil) min"
+        }
+
+        cell.detailTextLabel?.text = detail
+
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
 }
