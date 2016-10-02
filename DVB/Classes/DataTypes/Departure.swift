ad8adc5a6d851eda0359c341d23121aacdfb9f2c
Kilian Koeltzsch <me@kilian.io>
2016-10-02 23:30:03 +0200

    some Departure doc refactoring

--- DVB/Classes/DataTypes/Departure.swift
+++ DVB/Classes/DataTypes/Departure.swift
@@ -8,45 +8,49 @@
 
 import Foundation
 
-/**
- *  A bus, tram or whatever leaving a specific stop at a specific time.
- */
+/// A bus, tram or whatever leaving a specific stop at a specific time
 public struct Departure {
 
-    /// Line identifier, e.g. "3", "85" or "EV3".
+    /// Line identifier, e.g. "3", "85" or "EV3"
     public let line: String
 
     /// Type of the Departure
+    /// - warning: This is only a best guess approximation and might fail. Substitute lines are especially hard.
+    ///   Should you ever find inconsistencies (especially if nil is returned), please tell me about it and open an issue. Thanks! 🙂 
+    ///   https://github.com/kiliankoe/DVB/issues/new
     public var type: TransportMode.Departures? {
         return parseType()
     }
 
-    /// Destination of the departure, e.g. "Bühlau" or "Wilder Mann".
+    /// Destination of the departure, e.g. "Bühlau", "Wilder Mann" etc.
     public let direction: String
 
-    /// How many minutes until the departure leaves.
+    /// How many minutes until the departure leaves
     public let minutesUntil: Int
 
-    /// Convenience getter for minutesUntil as NSDate
+    /// Convenience getter for minutesUntil as Date
     public var leavingDate: Date {
         return Date().addingTimeInterval(Double(minutesUntil) * 60)
     }
 
-    /**
-     Init a Departure
-
-     - parameter line:         Line identifier, e.g. "3", "85" or "EV3".
-     - parameter direction:    Destination of the departure, e.g. "Bühlau" or "Wilder Mann".
-     - parameter minutesUntil: How many minutes until the departure leaves.
-
-     - returns: new Departure
-     */
+    /// Initialize a Departure
+    ///
+    /// - parameter line:         Line identifier, e.g. "3", "85", "EV7", etc.
+    /// - parameter direction:    Destination of the departure, e.g. "Bühlau", "Wilder Mann", etc.
+    /// - parameter minutesUntil: Minutes until departure
+    ///
+    /// - returns: Departure
     public init(line: String, direction: String, minutesUntil: Int) {
         self.line = line
         self.direction = direction
         self.minutesUntil = minutesUntil
     }
 
+    /// Internal convenience initializer from a JSON object
+    ///
+    /// - parameter json: JSON object
+    ///
+    /// - returns: Departure
     init?(json: Any) {
         guard let list = json as? [String] else { return nil }
         guard list.count >= 3 else { return nil }
