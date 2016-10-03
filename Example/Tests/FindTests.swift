2dbca163139324d186a9e945a75e4c8362e10a01
Kilian Koeltzsch <me@kilian.io>
2016-10-03 14:43:21 +0200

    Refactor FindTests so as not to crash test suite
    because force unwrapping is always a good idea 🙈

--- Example/Tests/FindTests.swift
+++ Example/Tests/FindTests.swift
@@ -20,15 +20,24 @@ class FindTests: QuickSpec {
 
             it("should return the correct stops") {
                 let stops = DVB.find(query: "Zellesch")
-                expect(stops.first!.name) == "Zellescher Weg"
+                if let first = stops.first {
+                    expect(first.name) == "Zellescher Weg"
+                } else {
+                    fail("Got empty list")
+                }
             }
         }
 
         describe("DVB.nearestStops") {
             it("should return correct results") {
-                let (helmholtz, distance) = DVB.findNear(latitude: 51.0271761, longitude: 13.7258114, radius: 300).first!
-                expect(helmholtz.name) == "Helmholtzstraße"
-				expect(distance) <= 300
+                let found = DVB.findNear(latitude: 51.0271761, longitude: 13.7258114, radius: 300)
+                if let first = found.first {
+                    let (helmholtz, distance) = first
+                    expect(helmholtz.name) == "Helmholtzstraße"
+                    expect(distance) <= 300
+                } else {
+                    fail("Got empty list")
+                }
             }
         }
     }
