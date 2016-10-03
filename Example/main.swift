47ce901d472974483fd7670eef9bd04dec258556
Kilian Koeltzsch <me@kilian.io>
2016-10-03 22:49:31 +0200

    fix example code syntax errors
    that's what you get for not using Xcode I guess

--- Example/main.swift
+++ Example/main.swift
@@ -8,14 +8,14 @@ DVB.departures("Postplatz") { departures, error in
   }
 }
 
-let stops = DVB.find("Zellesch")
+let stops = DVB.find(query: "Zellesch")
 print(stops)
 
 let closeStops = DVB.findNear(latitude: 51.0271761, longitude: 13.7258114, radius: 300)
 print(closeStops)
 
 DVB.routeChanges { updated, routeChanges, error in
-    guard error = nil else { print("Couldn't get route changes: \(error)"); return }
+    guard error == nil else { print("Couldn't get route changes: \(error)"); return }
     print(updated)
     print(routeChanges)
 }
