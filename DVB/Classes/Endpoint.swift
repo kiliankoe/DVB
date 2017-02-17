768025d15ba860e8f0ea0a123ba6911f7df1ef5d
Kilian Koeltzsch <me@kilian.io>
2017-02-17 23:30:11 +0100

    Replace URL by Endpoint

---
+++ DVB/Classes/Endpoint.swift
@@ -0,0 +1,14 @@
+//
+//  URL.swift
+//  Pods
+//
+//  Created by Kilian Költzsch on 06/05/16.
+//
+//
+
+import Foundation
+
+enum Endpoint {
+    static let base = URL(string: "https://webapi.vvo-online.de/")!
+    static let pointfinder = URL(string: "tr/pointfinder", relativeTo: Endpoint.base)!
+}
