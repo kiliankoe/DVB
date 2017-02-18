e671bab8c2db0bffbfa12ddde0536b8de6c2d58c
Kilian Koeltzsch <me@kilian.io>
2017-02-18 02:01:09 +0100

    Diva: FromJson

--- DVB/Classes/DataTypes/Diva.swift
+++ DVB/Classes/DataTypes/Diva.swift
@@ -12,3 +12,14 @@ public struct Diva {
     let number: String
     let network: String
 }
+
+extension Diva: FromJSON {
+    init?(json: JSON) {
+        guard let number = json["Number"] as? String,
+            let network = json["Network"] as? String else {
+                return nil
+        }
+        self.number = number
+        self.network = network
+    }
+}
