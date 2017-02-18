c42202c5fe828c4f99085f118787af91fc8d0733
Kilian Koeltzsch <me@kilian.io>
2017-02-18 02:33:38 +0100

    pure genius generic post func, this is fun!

--- DVB/Classes/Network.swift
+++ DVB/Classes/Network.swift
@@ -8,37 +8,45 @@
 
 import Foundation
 
-func get(_ url: URL, raw: Bool = false, completion: @escaping (Result<Any, DVBError>) -> Void) {
+func get(_ url: URL, completion: @escaping (Result<Any, DVBError>) -> Void) {
     var request = URLRequest(url: url)
     request.httpMethod = "GET"
-    dataTask(request: request, raw: raw, completion: completion)
+    dataTask(request: request, completion: completion)
 }
 
-func post(_ url: URL, data: [String: Any], raw: Bool = false, completion: @escaping (Result<Any, DVBError>) -> Void) {
+func post(_ url: URL, data: [String: Any], completion: @escaping (Result<Any, DVBError>) -> Void) {
     var request = URLRequest(url: url)
     request.httpMethod = "POST"
-    request.httpBody = try! JSONSerialization.data(withJSONObject: data)
-    dataTask(request: request, raw: raw, completion: completion)
+    request.httpBody = try? JSONSerialization.data(withJSONObject: data)
+    dataTask(request: request, completion: completion)
 }
 
-private func dataTask(request: URLRequest, raw: Bool, completion: @escaping (Result<Any, DVBError>) -> Void) {
+func post<T: FromJSON>(_ url: URL, data: [String: Any], completion: @escaping (Result<T, DVBError>) -> Void) {
+    post(url, data: data) { result in
+        switch result {
+        case .failure(let err):
+            completion(.failure(err))
+        case .success(let json):
+            guard let json = json as? JSON else { completion(.failure(.decode)); return }
+            guard let resp = T(json: json) else { completion(.failure(.decode)); return }
+            completion(.success(resp))
+        }
+    }
+}
+
+private func dataTask(request: URLRequest, completion: @escaping (Result<Any, DVBError>) -> Void) {
     let session = URLSession(configuration: .default)
     session.dataTask(with: request) { data, response, error in
-        guard let data = data else { completion(.failure(error: .request)); return }
+        guard let data = data else { completion(.failure(.request)); return }
 
         if let resp = response as? HTTPURLResponse, resp.statusCode / 100 != 2 {
-            completion(.failure(error: .server(statusCode: resp.statusCode)))
+            completion(.failure(.server(statusCode: resp.statusCode)))
             return
         }
 
-        if raw {
-            completion(.success(value: data))
-            return
-        }
+        let rawJson = try? JSONSerialization.jsonObject(with: data)
 
-        let rawJson = try? JSONSerialization.jsonObject(with: data, options: .allowFragments)
-
-        guard let json = rawJson else { completion(.failure(error: .decode)); return }
+        guard let json = rawJson else { completion(.failure(.decode)); return }
 
         completion(.success(value: json))
     }.resume()
