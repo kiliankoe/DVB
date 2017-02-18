105db61f8b806b0959067386e08330f9f66d94b9
Kilian Koeltzsch <me@kilian.io>
2017-02-18 01:16:29 +0100

    add departure monitor URL

--- DVB/Classes/Endpoint.swift
+++ DVB/Classes/Endpoint.swift
@@ -11,4 +11,5 @@ import Foundation
 enum Endpoint {
     static let base = URL(string: "https://webapi.vvo-online.de/")!
     static let pointfinder = URL(string: "tr/pointfinder", relativeTo: Endpoint.base)!
+    static let departureMonitor = URL(string: "dm", relativeTo: Endpoint.base)!
 }
