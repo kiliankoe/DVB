f33fc3749c612274a62a46d0977456dcee69b845
Kilian Koeltzsch <me@kilian.io>
2017-02-18 11:25:24 +0100

    add some convenience stuff to result

--- DVB/Classes/DataTypes/Result.swift
+++ DVB/Classes/DataTypes/Result.swift
@@ -8,7 +8,35 @@
 
 import Foundation
 
-public enum Result<Val, Err: Error> {
-    case success(Val)
+public enum Result<Value, Err: Error> {
+    case success(Value)
     case failure(Err)
+
+    public func get() throws -> Value {
+        switch self {
+        case .success(let x): return x
+        case .failure(let e): throw e
+        }
+    }
+
+    public var success: Value? {
+        switch self {
+        case .success(let x): return x
+        case .failure(_): return nil
+        }
+    }
+
+    public var failure: Err? {
+        switch self {
+        case .success(_): return nil
+        case .failure(let e): return e
+        }
+    }
+}
+
+public func ?? <T,E>(result: Result<T,E>, defaultValue: @autoclosure () -> T) -> T {
+    switch result {
+    case .success(let x): return x
+    case .failure(_): return defaultValue()
+    }
 }
