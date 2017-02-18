0bfa327e277cb8f95dcf4871b3544c82535d529e
Kilian Koeltzsch <me@kilian.io>
2017-02-18 02:59:48 +0100

    comment out and delete obsolete tests

--- Example/Tests/DeparturesTests.swift
+++ Example/Tests/DeparturesTests.swift
@@ -13,28 +13,28 @@ import DVB
 class DeparturesTests: QuickSpec {
     override func spec() {
         describe("DVB.departures") {
-            it("should return results") {
-                DVB.departures("Postplatz", completion: { departures, err in
-                    guard err == nil else { fail("Received an API error"); return }
-                    expect(departures.count) > 0
-                })
-            }
-
-            it("shouldn't find results for unknown stop") {
-                DVB.departures("foobarbaz", completion: { departures, err in
-                    guard err == nil else { fail("Received an API error"); return }
-                    expect(departures.count) == 0
-                })
-            }
-
-            it("should only return requested lines") {
-                DVB.departures("Pirnaischer Platz", line: ["3"], completion: { departures, err in
-                    guard err == nil else { fail("Received an API error"); return }
-                    for dep in departures {
-                        expect(dep.line) == "3"
-                    }
-                })
-            }
+//            it("should return results") {
+//                DVB.departures("Postplatz", completion: { departures, err in
+//                    guard err == nil else { fail("Received an API error"); return }
+//                    expect(departures.count) > 0
+//                })
+//            }
+//
+//            it("shouldn't find results for unknown stop") {
+//                DVB.departures("foobarbaz", completion: { departures, err in
+//                    guard err == nil else { fail("Received an API error"); return }
+//                    expect(departures.count) == 0
+//                })
+//            }
+//
+//            it("should only return requested lines") {
+//                DVB.departures("Pirnaischer Platz", line: ["3"], completion: { departures, err in
+//                    guard err == nil else { fail("Received an API error"); return }
+//                    for dep in departures {
+//                        expect(dep.line) == "3"
+//                    }
+//                })
+//            }
         }
     }
 }
--- Example/Tests/StopTests.swift
+++ Example/Tests/StopTests.swift
@@ -12,16 +12,16 @@ import DVB
 
 class StopTests: QuickSpec {
     override func spec() {
-        describe("Stop") {
-            let stop = Stop(id: 0, name: "Helmholtzstraße", region: "Dresden", searchString: "", tarifZones: "", longitude: 1.0, latitude: 1.0, priority: 1)
-
-            it("should have the correct name") {
-                expect(stop.name) == "Helmholtzstraße"
-            }
-
-            it("should have the correct location") {
-                expect(stop.region) == "Dresden"
-            }
-        }
+//        describe("Stop") {
+//            let stop = Stop(id: 0, name: "Helmholtzstraße", region: "Dresden", searchString: "", tarifZones: "", longitude: 1.0, latitude: 1.0, priority: 1)
+//
+//            it("should have the correct name") {
+//                expect(stop.name) == "Helmholtzstraße"
+//            }
+//
+//            it("should have the correct location") {
+//                expect(stop.region) == "Dresden"
+//            }
+//        }
     }
 }
