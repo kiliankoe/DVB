a83208cb4b02bf409e8d2c581458d29c2a925bf4
Kilian Koeltzsch <me@kilian.io>
2017-02-19 12:36:25 +0100

    list testcases in LinuxMain

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,6 +1,12 @@
 import XCTest
-@testable import DVBTests
+@testable import StopTests
+@testable import DepartureTests
+@testable import FindTests
+@testable import MonitorTests
 
 XCTMain([
-    testCase(DVBTests.allTests),
+    testCase(StopTests.allTests),
+    testCase(DepartureTests.allTests),
+    testCase(FindTests.allTests),
+    testCase(MonitorTests.allTests),
 ])
