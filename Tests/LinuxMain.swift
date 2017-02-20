5c402e6b7231ba0873793bf00e9cb140eab05e83
Kilian Koeltzsch <me@kilian.io>
2017-02-20 19:26:26 +0100

    add missing tests to linuxmain.swift

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,12 +1,18 @@
 import XCTest
 @testable import StopTests
-@testable import DepartureTests
 @testable import FindTests
+@testable import DepartureTests
+@testable import LineTests
 @testable import MonitorTests
+@testable import RouteChangeTests
+@testable import RouteChangesTests
 
 XCTMain([
     testCase(StopTests.allTests),
-    testCase(DepartureTests.allTests),
     testCase(FindTests.allTests),
+    testCase(DepartureTests.allTests),
+    testCase(LineTests.allTests),
     testCase(MonitorTests.allTests),
+    testCase(RouteChangeTests.allTests),
+    testCase(RouteChangesTests.allTests)
 ])
