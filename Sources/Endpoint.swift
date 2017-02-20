1519d817167aded6e189be989905a8fcb2cec31d
Kilian Koeltzsch <me@kilian.io>
2017-02-20 01:18:16 +0100

    add known endpoint URLs

--- Sources/Endpoint.swift
+++ Sources/Endpoint.swift
@@ -2,6 +2,20 @@ import Foundation
 
 enum Endpoint {
     static let base = URL(string: "https://webapi.vvo-online.de/")!
-    static let pointfinder = URL(string: "tr/pointfinder", relativeTo: Endpoint.base)!
-    static let departureMonitor = URL(string: "dm", relativeTo: Endpoint.base)!
+
+    // swiftlint:disable:next variable_name
+    private static let tr = URL(string: "tr/", relativeTo: Endpoint.base)!
+    static let pointfinder = URL(string: "pointfinder", relativeTo: Endpoint.tr)!
+    static let trips = URL(string: "trips", relativeTo: Endpoint.tr)!
+
+    static let departureMonitor = URL(string: "dm/", relativeTo: Endpoint.base)!
+    static let perlschnur = URL(string: "trip", relativeTo: Endpoint.departureMonitor)!
+
+    private static let stt = URL(string: "stt/", relativeTo: Endpoint.base)!
+    static let lines = URL(string: "lines", relativeTo: Endpoint.stt)!
+
+    private static let map = URL(string: "map/", relativeTo: Endpoint.base)!
+    static let mapPins = URL(string: "pins", relativeTo: Endpoint.map)!
+
+    static let routeChanges = URL(string: "rc", relativeTo: Endpoint.base)!
 }
