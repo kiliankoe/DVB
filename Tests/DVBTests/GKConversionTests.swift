4f26e421e1b2caa61e7bf366455d5ee4b80d5900
Kilian Koeltzsch <me@kilian.io>
2017-02-23 22:03:23 +0100

    Add coordinate conversion code w/ tests

---
+++ Tests/DVBTests/GKConversionTests.swift
@@ -0,0 +1,22 @@
+import Foundation
+import XCTest
+import CoreLocation
+@testable import DVB
+
+class GKConversionTests: XCTestCase {
+    func testGKtoWGS() {
+        let gk = (x: 4591270.0, y: 5819620.0)
+        guard let wgs = CLLocationCoordinate2D(x: gk.x, y: gk.y) else { XCTFail(); return }
+
+        XCTAssertEqual(wgs.latitude, 52.502133988116455)
+        XCTAssertEqual(wgs.longitude, 13.342517405215336)
+    }
+
+    func testWGStoGK() {
+        let wgs = CLLocationCoordinate2D(latitude: 52.502133988116455, longitude: 13.342517405215336)
+        guard let gk = wgs.asGK else { XCTFail(); return }
+
+        XCTAssertEqual(gk.x, 4591270)
+        XCTAssertEqual(gk.y, 5819620)
+    }
+}
