1c7af8da1dc4a0db9d00699dbd26aa5004230ef7
Kilian Koeltzsch <me@kilian.io>
2017-02-27 02:53:46 +0100

    Rename some endpoints

--- Sources/Endpoint.swift
+++ Sources/Endpoint.swift
@@ -6,16 +6,16 @@ enum Endpoint {
     // swiftlint:disable:next variable_name
     private static let tr = URL(string: "tr/", relativeTo: Endpoint.base)!
     static let pointfinder = URL(string: "pointfinder", relativeTo: Endpoint.tr)!
-    static let trips = URL(string: "trips", relativeTo: Endpoint.tr)!
+    static let routing = URL(string: "trips", relativeTo: Endpoint.tr)!
 
     static let departureMonitor = URL(string: "dm/", relativeTo: Endpoint.base)!
-    static let perlschnur = URL(string: "trip", relativeTo: Endpoint.departureMonitor)!
+    static let routeMap = URL(string: "trip", relativeTo: Endpoint.departureMonitor)!
 
     private static let stt = URL(string: "stt/", relativeTo: Endpoint.base)!
     static let lines = URL(string: "lines", relativeTo: Endpoint.stt)!
 
     private static let map = URL(string: "map/", relativeTo: Endpoint.base)!
-    static let mapPins = URL(string: "pins", relativeTo: Endpoint.map)!
+    static let poiSearch = URL(string: "pins", relativeTo: Endpoint.map)!
 
     static let routeChanges = URL(string: "rc", relativeTo: Endpoint.base)!
 }
