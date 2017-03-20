71d7daff85e5fabfb3ed952c4acf0e07e67a065c
Kilian Koeltzsch <me@kilian.io>
2017-03-20 03:38:26 +0100

    add gausskrueger@0.2.1

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "DVB",
 	dependencies: [
-		.Package(url: "https://github.com/utahiosmac/Marshal", majorVersion: 1, minor: 2)
+		.Package(url: "https://github.com/utahiosmac/Marshal", majorVersion: 1, minor: 2),
+        .Package(url: "https://github.com/kiliankoe/gausskrueger", majorVersion: 0, minor: 2),
 	]
 )
