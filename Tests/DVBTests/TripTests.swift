4e439f1af77efe454b18625d3a041025319fb0fb
Kilian Koeltzsch <me@kilian.io>
2017-03-20 02:44:14 +0100

    add trip test for convenience func

--- Tests/DVBTests/TripTests.swift
+++ Tests/DVBTests/TripTests.swift
@@ -6,7 +6,26 @@ class TripTests: XCTestCase {
     func testFromAlbertToHbf() {
         let e = expectation(description: "Find a route")
 
-        Trip.find(originId: "33000013", destinationId: "33000028") { result in
+        Trip.find(fromWithID: "33000013", toWithID: "33000028") { result in
+            switch result {
+            case .failure(let error):
+                XCTFail("Failed with error: \(error)")
+            case .success(let response):
+                guard response.routes.count > 0 else {
+                    XCTFail("Found no routes")
+                    return
+                }
+                e.fulfill()
+            }
+        }
+
+        waitForExpectations(timeout: 5)
+    }
+
+    func testFromPostToPirnaischer() {
+        let e = expectation(description: "Find a route")
+
+        Trip.find(from: "Postplatz", to: "Pirnaischer Platz") { result in
             switch result {
             case .failure(let error):
                 XCTFail("Failed with error: \(error)")
