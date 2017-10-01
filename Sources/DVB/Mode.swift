b30ab7e13d2367124fc02b0501e2bead8ae524be
Kilian Koeltzsch <me@kilian.io>
2017-10-01 22:54:30 +0200

    update Mode
    Also a struct with static members now. New cases could come,
    who knows... Removed a few redundant copmuted vars as well.

--- Sources/DVB/Mode.swift
+++ Sources/DVB/Mode.swift
@@ -1,39 +1,38 @@
 import Foundation
 
-public enum Mode: String {
-    // swiftlint:disable:next identifier_name
-    case Tram, CityBus, IntercityBus, SuburbanRailway, Train, Cableway, Ferry, HailedSharedTaxi
+public struct Mode: Decodable {
+    public let identifier: String
 
-    static var all: [Mode] {
-        return [.Tram, .CityBus, .IntercityBus, .SuburbanRailway, .Train, .Cableway, .Ferry, .HailedSharedTaxi]
-    }
+    public static let Tram = Mode(value: "tram")
+    public static let CityBus = Mode(value: "citybus")
+    public static let IntercityBus = Mode(value: "bus")
+    public static let SuburbanRailway = Mode(value: "metropolitan")
+    public static let Train = Mode(value: "train")
+    public static let Cableway = Mode(value: "lift")
+    public static let Ferry = Mode(value: "Ferry")
+    public static let HailedSharedTaxi = Mode(value: "alita")
 
-    public var identifier: String {
-        return self.rawValue
-    }
-
-    public var dvbIdentifier: String {
-        switch self {
-        case .Tram:
-            return "tram"
-        case .CityBus:
-            return "citybus"
-        case .IntercityBus:
-            return "bus"
-        case .SuburbanRailway:
-            return "metropolitan"
-        case .Train:
-            return "train"
-        case .Cableway:
-            return "lift"
-        case .Ferry:
-            return "ferry"
-        case .HailedSharedTaxi:
-            return "alita"
+    init(value: String) {
+        switch value.lowercased() {
+        case Mode.Tram.identifier: self = Mode.Tram
+        case Mode.CityBus.identifier: self = Mode.CityBus
+        case Mode.IntercityBus.identifier, "intercitybus": self = Mode.IntercityBus
+        case Mode.SuburbanRailway.identifier, "suburbanrailway": self = Mode.SuburbanRailway
+        case Mode.Train.identifier: self = Mode.Train
+        case Mode.Cableway.identifier, "cableway": self = Mode.Cableway
+        case Mode.Ferry.identifier: self = Mode.Ferry
+        case Mode.HailedSharedTaxi.identifier, "hailedsharedtaxi": self = Mode.HailedSharedTaxi
+        default:
+            self.identifier = value
         }
     }
 
-    public var iconURL: URL {
-        return URL(string: "https://www.dvb.de/assets/img/trans-icon/transport-\(self.dvbIdentifier).svg")!
+    public static var all: [Mode] {
+        return [.Tram, .CityBus, .IntercityBus, .SuburbanRailway, .Train, .Cableway, .Ferry, .HailedSharedTaxi]
+    }
+
+    public var iconURL: URL? {
+        guard let identifier = self.identifier.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) else { return nil }
+        return URL(string: "https://www.dvb.de/assets/trans-icon/transport-\(identifier).svg")
     }
 }
