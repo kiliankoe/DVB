2d6a3c4d10f0415f8ff0ff4e30484e33fa803364
Kilian Koeltzsch <me@kilian.io>
2017-10-01 23:12:56 +0200

    Migrate POI
    Refactor POI.Kind to struct with static members

--- Sources/DVB/POI.swift
+++ Sources/DVB/POI.swift
@@ -1,42 +1,40 @@
 import Foundation
 import GaussKrueger
 
-public struct POIResponse {
+public struct POIResponse: Decodable {
     public let pins: [POI]
     public let expirationTime: Date
+
+    private enum CodingKeys: String, CodingKey {
+        case pins = "Pins"
+        case expirationTime = "ExpirationTime"
+    }
 }
 
-public struct POI {
+public struct POI: Decodable {
     public let descriptionString: String // FIXME: ‼️
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        self.descriptionString = try container.decode(String.self)
+    }
 }
 
 extension POI {
-    public enum Kind: String {
-        // swiftlint:disable:next identifier_name
-        case RentABike, Stop, Poi, CarSharing, TicketMachine, Platform, ParkAndRide
+    public struct Kind {
+        public let rawValue: String
+
+        public let RentABike = Kind(rawValue: "RentABike")
+        public let Stop = Kind(rawValue: "Stop")
+        public let Poi = Kind(rawValue: "Poi")
+        public let CarSharing = Kind(rawValue: "CarSharing")
+        public let TicketMachine = Kind(rawValue: "TicketMachine")
+        public let Platform = Kind(rawValue: "Platform")
+        public let ParkAndRide = Kind(rawValue: "ParkAndRide")
 
         public static var all: [Kind] {
             return [.RentABike, .Stop, .Poi, .CarSharing, .TicketMachine, .Platform, .ParkAndRide]
         }
-    }
-}
-
-// MARK: - JSON
-
-extension POIResponse: Unmarshaling {
-    public init(object: MarshaledObject) throws {
-        pins = try object <| "Pins"
-        expirationTime = try object <| "ExpirationTime"
-    }
-}
-
-extension POI: ValueType {
-    public static func value(from object: Any) throws -> POI {
-        guard let rawVal = object as? String else {
-            throw MarshalError.typeMismatch(expected: String.self, actual: type(of: object))
-        }
-
-        return self.init(descriptionString: rawVal)
     }
 }
 
@@ -48,7 +46,10 @@ extension POI {
         public let southwest: Coordinate
     }
 
-    public static func find(types: [POI.Kind] = POI.Kind.all, in rect: CoordRect, session: URLSession = .shared, completion: @escaping (Result<POIResponse>) -> Void) {
+    public static func find(types: [POI.Kind] = POI.Kind.all,
+                            in rect: CoordRect,
+                            session: URLSession = .shared,
+                            completion: @escaping (Result<POIResponse>) -> Void) {
         guard
             let gkSWCoord = rect.southwest.asGK,
             let gkNECoord = rect.northeast.asGK
