119e6850ab7c3c7078d77f613814ab73e3870863
Kilian Koeltzsch <me@kilian.io>
2017-10-02 00:53:24 +0200

    migrate Trip

--- Sources/DVB/Trip.swift
+++ Sources/DVB/Trip.swift
@@ -1,52 +1,52 @@
 import Foundation
 
-public struct TripsResponse {
+public struct TripsResponse: Decodable {
     public let stops: [TripStop]
     public let expirationTime: Date
+
+    private enum CodingKeys: String, CodingKey {
+        case stops = "Stops"
+        case expirationTime = "ExpirationTime"
+    }
 }
 
-public struct TripStop {
-    public enum Position: String {
-        // swiftlint:disable:next identifier_name
-        case Previous, Current, Next
-    }
-
+public struct TripStop: Decodable {
     public let id: String
     public let place: String
     public let name: String
     public let position: Position
     public let platform: Platform?
     public let time: Date
-}
 
-// MARK: - JSON
-
-extension TripsResponse: Unmarshaling {
-    public init(object: MarshaledObject) throws {
-        stops = try object <| "Stops"
-        expirationTime = try object <| "ExpirationTime"
+    private enum CodingKeys: String, CodingKey {
+        case id = "Id"
+        case place = "Place"
+        case name = "Name"
+        case position = "Position"
+        case platform = "Platform"
+        case time = "Time"
     }
 }
 
-extension TripStop: Unmarshaling {
-    public init(object: MarshaledObject) throws {
-        id = try object <| "Id"
-        place = try object <| "Place"
-        name = try object <| "Name"
-        position = try object <| "Position"
-        platform = try object <| "Platform"
-        time = try object <| "Time"
+extension TripStop {
+    public enum Position: String, Decodable {
+        // swiftlint:disable:next identifier_name
+        case Previous, Current, Next
     }
 }
 
 // MARK: - API
 
 extension TripStop {
-    public static func get(forTripID tripID: String, stopID: String, atTime time: Date, session: URLSession = .shared, completion: @escaping (Result<TripsResponse>) -> Void) {
+    public static func get(forTripID tripID: String,
+                           stopID: String,
+                           atTime time: Date,
+                           session: URLSession = .shared,
+                           completion: @escaping (Result<TripsResponse>) -> Void) {
         let data: [String: Any] = [
             "tripid": tripID,
             "stopid": stopID,
-            "time": time.datestring,
+            "time": time.sapPattern,
         ]
 
         post(Endpoint.trip, data: data, session: session, completion: completion)
